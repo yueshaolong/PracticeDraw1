5fcf4f4d0f34aa4062f6bb731908d82ac22f2993
yueshaolong <yueshaolong@vsxin.com>
2019-02-26 17:24:24 +0800

    练习画图

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,8 +26,34 @@ public class Practice10HistogramView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        Paint paint = new Paint();
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        paint.setColor(Color.WHITE);
+        canvas.drawLine(200, 200, 200, 600, paint);
+        canvas.drawLine(200, 600, 1000, 600, paint);
+        paint.setStrokeWidth(2);
+        canvas.drawLine(220, 598, 300, 598, paint);
+        paint.setStyle(Style.FILL);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(320,580, 400, 600,paint);
+        canvas.drawRect(420,580, 500, 600,paint);
+        canvas.drawRect(520,460, 600, 600,paint);
+        canvas.drawRect(620,300, 700, 600,paint);
+        canvas.drawRect(720,260, 800, 600,paint);
+        canvas.drawRect(820,420, 900, 600,paint);
+        paint.setTextSize(25);
+        paint.setColor(Color.WHITE);
+        canvas.drawText("Froyo", 0, "Froyo".length(), 220, 625, paint);
+        canvas.drawText("GB", 0, "GB".length(), 340, 625, paint);
+        canvas.drawText("ICS", 0, "ICS".length(), 440, 625, paint);
+        canvas.drawText("JB", 0, "JB".length(), 540, 625, paint);
+        canvas.drawText("KitKat", 0, "KitKat".length(), 630, 625, paint);
+        canvas.drawText("L", 0, "L".length(), 760, 625, paint);
+        canvas.drawText("M", 0, "M".length(), 860, 625, paint);
+        paint.setTextSize(40);
+        canvas.drawText("直方图", 0, "直方图".length(), 520, 725, paint);
+
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,9 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw1.R;
 
 public class Practice11PieChartView extends View {
 
@@ -26,5 +33,35 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        Paint paint = new Paint();
+
+        RectF rectF = new RectF(300, 200, 900, 800);
+        paint.setColor(Color.YELLOW);
+        canvas.drawArc(rectF, -45, 40, true, paint);
+        paint.setColor(getResources().getColor(R.color.zi));
+        canvas.drawArc(rectF, 0, 15, true, paint);
+        paint.setColor(Color.GRAY);
+        canvas.drawArc(rectF, 17, 10, true, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(rectF, 29, 45, true, paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(rectF, 76, 104, true, paint);
+
+        paint.setColor(Color.RED);
+        RectF rectF1 = new RectF(280, 180, 880, 780);
+//        canvas.drawArc(280, 180, 880, 780, -180, 135, true, paint);
+        canvas.drawArc(rectF1, -180, 135, true, paint);
+
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Style.STROKE);
+        Path path = new Path();
+        path.moveTo(340, 320);
+        path.lineTo(290, 180);
+        path.rLineTo(-110, 0);
+        canvas.drawPath(path,paint);
+
+        paint.setTextSize(30);
+        canvas.drawText("Lollopop", 50, 190, paint);
+
     }
 }
